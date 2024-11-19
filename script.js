b5e213db64340f63238d8d87adf75e9aaa228124
Caroline Santana <opscarool01@gmail.com>
2024-11-19 20:18:16 -0300

    ESTRUTURA 5%

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("scroll", () => {
+    const scrollTop = window.scrollY; // Distância rolada da parte superior
+    const yellowBar = document.querySelector(".yellow-bar");
+  
+    // Ajusta a altura da barra amarela com base na rolagem
+    yellowBar.style.height = `${scrollTop}px`;
+  });
+  
