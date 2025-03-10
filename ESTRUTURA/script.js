ac1a420300e54baa8e58d883ceb65a506b0cfd7e
Caroline Santana <opscarool01@gmail.com>
2025-03-10 17:14:12 -0300

    BUTTON E MENU
    Foram adicionados o button e o menu de navegação.
    obs: pequena alteração no código com a remoção da logo que fazia a sobreposição.

--- ESTRUTURA/script.js
+++ ESTRUTURA/script.js
@@ -1,3 +1,4 @@
+// HEAD
 setTimeout(() => {
     document.getElementById("imagem").style.opacity = "1";
 }, 4000);
@@ -20,3 +21,47 @@ window.addEventListener("scroll", function() {
         hero.style.opacity = "1";
     }
 });
+
+// BUTTON
+window.addEventListener("scroll", function() {
+    let button = document.getElementById("orcamento");
+
+    if (window.scrollY > 100) {
+        button.style.display = "block";
+        button.style.opacity = "1";
+    } else {
+        button.style.opacity = "0";
+        setTimeout(() => { button.style.display = "none"; }, 500);
+    }
+});
+// MENU
+window.addEventListener("scroll", function() {
+    let header = document.getElementById("header");
+    let logo = document.getElementById("logo");
+    let hero = document.getElementById("hero");
+    let button = document.getElementById("orcamento");
+    let menu = document.querySelector(".menu");
+
+    if (window.scrollY > 100) {
+        header.style.height = "80px";
+        logo.style.width = "100px";
+        logo.style.height = "100px";
+        hero.style.opacity = "0";
+        hero.style.transition = "opacity 1s ease-in-out";
+        button.style.display = "block";
+        button.style.opacity = "1";
+        menu.style.opacity = "1";
+        menu.style.visibility = "visible";
+    } else {
+        header.style.height = "100px";
+        logo.style.width = "200px";
+        logo.style.height = "200px";
+        hero.style.opacity = "1";
+        button.style.opacity = "0";
+        setTimeout(() => { button.style.display = "none"; }, 500);
+        menu.style.opacity = "0";
+        menu.style.visibility = "hidden";
+    }
+});
+
+  
