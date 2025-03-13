b2de492b14b5a77708a27063a919b2561273f3bc
Caroline Santana <opscarool01@gmail.com>
2025-03-13 00:12:00 -0300

    Alterações no botão

--- ESTRUTURA/script.js
+++ ESTRUTURA/script.js
@@ -8,29 +8,26 @@ window.addEventListener("scroll", function() {
     let button = document.getElementById("orcamento");
 
     if (window.scrollY > 50) {
-        button.style.display = "block";
-        button.style.opacity = "1";
+        button.style.transform = "scale(0.8)"; // Diminui ao rolar a página
     } else {
-        button.style.opacity = "1";
-        setTimeout(() => { button.style.display = "none"; }, 500);
+        button.style.transform = "scale(1)"; // Tamanho normal ao topo
     }
 });
 
-// MENU
+// MENU & LOGO
 window.addEventListener("scroll", function() {
     let header = document.getElementById("header");
     let logo = document.getElementById("logo");
     let hero = document.getElementById("hero");
     let button = document.getElementById("orcamento");
-    
+    let menu = document.getElementById("menu");
+
     if (window.scrollY > 100) {
         header.style.height = "25px";
         logo.style.width = "100px";
         logo.style.height = "92px";
+        logo.style.transition = "all 0.5s ease-in-out";
         hero.style.opacity = "1";
-        hero.style.transition = "opacity 1s ease-in-out";
-        button.style.display = "block";
-        button.style.opacity = "1";
         menu.style.opacity = "1";
         menu.style.visibility = "visible";
     } else {
@@ -38,8 +35,6 @@ window.addEventListener("scroll", function() {
         logo.style.width = "170px";
         logo.style.height = "166px";
         hero.style.opacity = "1";
-        button.style.opacity = "0";
-        setTimeout(() => { button.style.display = "none"; }, 500);
         menu.style.opacity = "0";
         menu.style.visibility = "hidden";
     }
